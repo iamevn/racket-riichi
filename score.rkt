5d8ea1eb2d16a916fc378e2dccdd461a2ea4fdbc
iamevn <iamevn@users.noreply.github.com>
2020-09-16 00:13:18 -0700

    prep yaku
    list yaku, need to implement their scoring

--- score.rkt
+++ score.rkt
@@ -19,54 +19,95 @@
 ; TODO: actual scoring (mangan, haneman, etc)
 
 ; general game state around a win
-(struct/contract win-conditons ([dora-indicators (listof tile?)] ; list of dora indicators
-                                [tsumo? boolean?] ; win by self draw?
-                                [ron? boolean?] ; win by discard?
-                                [dealer? boolean?] ; winner is dealer?
-                                [haitei? boolean?] ; win on last draw?
-                                [houtei? boolean?] ; win on last discard?
-                                [chankan? boolean?] ; win by robbing kan?
-                                [rinshan? boolean?] ; win on deadwall draw after kan?
-                                ) #:transparent)
+(struct/contract gamestate ([dora-indicators (listof tile?)] ; list of dora indicators
+                            [tsumo? boolean?] ; win by self draw?
+                            [ron? boolean?] ; win by discard?
+                            [dealer? boolean?] ; winner is dealer?
+                            [haitei? boolean?] ; win on last draw?
+                            [houtei? boolean?] ; win on last discard?
+                            [chankan? boolean?] ; win by robbing kan?
+                            [rinshan? boolean?]) ; win on deadwall draw after kan?
+                 #:transparent)
 
-(define (make-conditions dora-indicators
-                         #:tsumo [tsumo #false]
-                         #:ron [ron #false]
-                         #:dealer [dealer #false]
-                         #:haitei [haitei #false]
-                         #:houtei [houtei #false]
-                         #:chankan [chankan #false]
-                         #:rinshan [rinshan #false])
+(define (make-gamestate dora-indicators
+                        #:tsumo [tsumo #false]
+                        #:ron [ron #false]
+                        #:dealer [dealer #false]
+                        #:haitei [haitei #false]
+                        #:houtei [houtei #false]
+                        #:chankan [chankan #false]
+                        #:rinshan [rinshan #false])
   (if (xor tsumo ron)
-      (win-conditons dora-indicators tsumo ron dealer houtei haitei chankan rinshan)
-      (raise-argument-error 'make-conditions
+      (gamestate dora-indicators tsumo ron dealer houtei haitei chankan rinshan)
+      (raise-argument-error 'make-gamestate
                             "tsumo or ron, not both"
                             (if (and tsumo ron)
                                 "#:tsumo #true #:ron #true"
                                 "#:tsumo #false #:ron #false"))))
 
+; yakulist
 (struct/contract yaku ([id symbol?]
+                       [description string?]
                        [han-open number?]
                        [han-closed number?]
-                       [in-hand? any/c]) #:transparent)
+                       [scoring (-> hand? gamestate? scoring?)]) #:transparent)
+(struct/contract yakuman ([id symbol?]
+                          [description string?]
+                          [available-open? boolean?]
+                          [scoring (-> hand? gamestate? scoring?)]) #:transparent)
 
-(define (idk-some-yaku)
-  ; closed only 1 han
-  '(riichi ippatsu tsumo pinfu iipeikou)
-  ; 1 han
-  '(haitei houtei rinshan chankan tanyao yakuhai)
-  ; 2 han
-  '(double-riichi chanta sanshoku-doujun ittsuu toitoi sanankou sanshoku-doukou sankantsu honroutou shousangen)
-  ; 3 han
-  '(honitsu junchan ryanpeikou)
-  ; 6 han
-  '(chinitsu)
-  ; yakuman
-  '(kazoe kokushi daisangen suuankou shousuushi daisuushi tsuuiisou ryuuiisou chinroutou chuuren suukantsu tenhou chiihou)
-  ; special
-  '(nagashi)
-  ; optional
-  '(renhou daisharin))
+(define yakulist
+  (list
+   (yaku 'menzen-tsumo "self-draw" 0 1 (λ (h g) dummy-score))
+   (yaku 'riichi "riichi" 0 1 (λ (h g) dummy-score))
+   (yaku 'ippatsu "one shot" 0 1 (λ (h g) dummy-score))
+   (yaku 'pinfu "no fu" 0 1 (λ (h g) dummy-score))
+   (yaku 'iipeikou "double sequences" 0 1 (λ (h g) dummy-score))
+   (yaku 'haitei "last draw" 1 1 (λ (h g) dummy-score))
+   (yaku 'houtei "last discard" 1 1 (λ (h g) dummy-score))
+   (yaku 'rinshan "dead wall draw" 1 1 (λ (h g) dummy-score))
+   (yaku 'chankan "robbing a kan" 1 1 (λ (h g) dummy-score))
+   (yaku 'tanyao "all simples" 1 1 (λ (h g) dummy-score))
+   (yaku 'yakuhai "value tiles" 1 1 (λ (h g) dummy-score))
+   (yaku 'double-riichi "double riichi" 2 2 (λ (h g) dummy-score))
+   (yaku 'chanta "half outside hand" 1 2 (λ (h g) dummy-score))
+   (yaku 'sanshoku-doujun "3 color straight" 1 2 (λ (h g) dummy-score))
+   (yaku 'ittsuu "straight" 1 2 (λ (h g) dummy-score))
+   (yaku 'toitoi "all triplets" 2 2 (λ (h g) dummy-score))
+   (yaku 'sanankou "three closed triplets" 2 2 (λ (h g) dummy-score))
+   (yaku 'sanshoku-doukou "three colored triplets" 2 2 (λ (h g) dummy-score))
+   (yaku 'sankantsu "three kans" 2 2 (λ (h g) dummy-score))
+   (yaku 'chiitoitsu "seven pairs" 0 2 (λ (h g) dummy-score))
+   (yaku 'honroutou "outside hand" 2 2 (λ (h g) dummy-score))
+   (yaku 'shousangen "small three dragons" 2 2 (λ (h g) dummy-score))
+   (yaku 'honitsu "half flush" 2 3 (λ (h g) dummy-score))
+   (yaku 'junchan "all terminals" 2 3 (λ (h g) dummy-score))
+   (yaku 'ryanpeikou "two double sequences" 0 3 (λ (h g) dummy-score))
+   (yaku 'chinitsu "flush" 5 6 (λ (h g) dummy-score))))
+
+(define yakumanlist
+  (list
+   (yakuman 'kazoe "counted yakuman" #true (λ (h g) dummy-score))
+   (yakuman 'kokushi-musou "thirteen orphans" #false (λ (h g) dummy-score))
+   (yakuman 'suuankou "four concealed triplets" #false (λ (h g) dummy-score))
+   (yakuman 'daisangen "big three dragons" #true (λ (h g) dummy-score))
+   (yakuman 'shousuushi "small four winds" #true (λ (h g) dummy-score))
+   (yakuman 'daisuushi "big four winds" #true (λ (h g) dummy-score))
+   (yakuman 'tsuuiisou "all honors" #true (λ (h g) dummy-score))
+   (yakuman 'chinroutou "all terminals" #true (λ (h g) dummy-score))
+   (yakuman 'ryuuiisou "all green" #true (λ (h g) dummy-score))
+   (yakuman 'chuuren "nine gates" #false (λ (h g) dummy-score))
+   (yakuman 'suukantsu "four kans" #true (λ (h g) dummy-score))
+
+   (yakuman 'tenhou "heavenly hand" #false (λ (h g) dummy-score))
+   (yakuman 'chiihou "earthly hand" #false (λ (h g) dummy-score))))
+
+; note:
+;  iipeikou doesn't score with ryanpeikou
+;  skipping optional yaku like renhou and daisharin
+;  don't forget nagashi mangan
+;  double yakuman
+
 
 ; hand scoring
 (define dummy-score (scoring 0 0 '("dummy-score")))
