1dd4dab5a3869eacba0287ad98e49b3ca01f24a3
iamevn <iamevn@users.noreply.github.com>
2020-09-29 13:35:43 -0700

    simplify server, clean up html generation
    especially make better use of quasiquotes (`) and unquote-splicing (,@)

--- web-test.rkt
+++ web-test.rkt
@@ -1,12 +1,7 @@
 #lang racket
 (require web-server/http
-         web-server/lang/web
-         web-server/servlet-dispatch
          web-server/servlet-env
-         web-server/lang/abort-resume
-         web-server/web-server
          web-server/servlet/setup
-         (prefix-in pathprocedure: web-server/dispatchers/dispatch-pathprocedure)
          net/url-structs
          net/url-string)
 (require "score-hand.rkt"
@@ -15,54 +10,49 @@
 
 ; from https://stackoverflow.com/questions/44863550/how-to-draw-a-picture-in-web-app-using-racket
 (define (img-encode img)
-  (format "data:image/png;base64,~a"
-          (base64-encode (convert img 'png-bytes))))
+  (~a "data:image/png;base64,"
+      (base64-encode (convert img 'png-bytes))))
 
 (define (gen-page hand gamestate)
   (let ([scorelist (list-score-hand hand gamestate)])
-    (λ (make-url)
-      (response/xexpr
-       (list 'html
-             (append*
-              `(body
-                (p ,(~a hand " south round, south seat, ron")
-                   (br)
-                   (br)))
-              (map (λ (img-and-text)
-                     (let* ([img (first img-and-text)]
-                            [text (second img-and-text)]
-                            [split-text (string-split text "\n")]
-                            [text-list-with-br (add-between split-text '(br))])
-                       `((img ([src ,(img-encode img)]))
-                         ,(cons 'p text-list-with-br)
-                         (br))))
-                   scorelist)))))))
+    (response/xexpr
+     `(html
+       (body
+        (p ,hand " south round, south seat, ron" (br) (br))
+        ,@(map (λ (img-and-text)
+                 (let* ([hand-image (first img-and-text)]
+                        [text (second img-and-text)]
+                        [split-text (string-split text "\n")])
+                   `(div
+                     (img ([src ,(img-encode hand-image)]))
+                     (p ,@(add-between split-text '(br)))
+                     (br))))
+               scorelist))))))
 
 (define (demo request)
-  (send/suspend/dispatch
-   ;(gen-page "456m11(1)22z 1111s 7777z" '(seat-s round-s ron))
-   (gen-page "22334455m44556(6)p" '(seat-s round-s ron))))
+  ;(gen-page "456m11(1)22z 1111s 7777z" '(seat-s round-s ron))
+  (gen-page "22334455m44556(6)p" '(seat-s round-s ron)))
 
 (define (hello request)
   (response/xexpr
-   `(html (body (p "hello world!" (br) (br) "demo page " (a ((href "demo")) "here"))))))
+   `(html (body (p "hello world!" (br) (br) "demo page " (a ([href "demo"]) "here"))))))
 
+(define (four-oh-four request)
+  (response/xexpr
+   `(html (body (p "unrecognized path:" (br) ,(url->string (request-uri request)))))))
 
-(define r '())
+(define r '()) ; for inspecting requests in repl
 (define (route request)
-  (display "updating r")(newline)(set! r request)(display r)(newline)
+  (set! r request) ; for inspecting requests in repl
   (let* ([uri (request-uri request)]
          [path (url-path uri)]
          [first-path (if (zero? (length path))
                          ""
                          (path/param-path (first path)))])
-    (cond
-      [(or (equal? first-path "hello")
-           (equal? first-path ""))
-       (hello request)]
-      [(equal? first-path "demo")
-       (demo request)]
-      [else (response/xexpr `(html (body (p "unrecognized path:" (br) ,(url->string uri)))))])))
+    (case first-path
+      [("hello" "") (hello request)]
+      [("demo") (demo request)]
+      [else (four-oh-four request)])))
 
 (define server
   (thread
