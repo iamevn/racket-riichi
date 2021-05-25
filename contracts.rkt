a7176947b2217b1c72daa858664f2458e0b50603
iamevn <iamevn@users.noreply.github.com>
2021-05-24 23:15:20 -0700

    contracts.rkt ported to typed/racket
    would like to add a types.rkt using these predicates to define actual types for these things

--- contracts.rkt
+++ contracts.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang typed/racket
 (provide tilelist?
          suit?
          tile?
@@ -11,29 +11,29 @@
 
 (require "util.rkt")
 
-(define (tilelist? hand)
+(define (tilelist? hand) : Boolean
   (and (list? hand)
        (or (empty? hand)
            (andmap tile? hand))))
 
-(define (suit? char)
+(define (suit? char) : Boolean
   (set-member? (set #\m #\p #\s #\z) char))
 
-(define (tile? tile)
+(define (tile? [tile : Any]) : Boolean
   (and (handstring? tile)
-       (equal? (string-length tile) 2)))
+       (equal? (string-length (assert tile string?)) 2)))
 
-(define (handstring? hand)
+(define (handstring? hand) : Boolean
   (and (string? hand)
        (regexp-match-exact? #rx"([1-9]+[msp]|[1-7]+z)*"
                             hand)))
 
-(define (strict-handstring? hand)
+(define (strict-handstring? [hand : String]) : Boolean
   (regexp-match-exact? #rx"([1-9][msp]|[1-7]z)*"
                        hand))
 
-(struct/contract short-yaku ([id symbol?] [value number?]) #:transparent)
-(struct/contract payment ([amount number?] [target symbol?]) #:transparent)
+(struct short-yaku ([id : Symbol] [value : Number]) #:transparent)
+(struct payment ([amount : Number] [target : Symbol]) #:transparent)
 
 ; normal tile notation followed by space separated list of calls
 ; calls notated like the following
@@ -49,8 +49,7 @@
 ;   "123234s555p88(8)m22z"
 ;   "123234s555p(8)88m22z"
 ;   "(8m)123234s555p88m22z"
-(define/contract (call-notation? s)
-  (-> any/c boolean?)
+(define (call-notation? s) : Boolean
   (and (string? s)
        (<= (count (curry equal? #\() (string->list s)) 1)
        (<= (count (curry equal? #\)) (string->list s)) 1)
@@ -58,5 +57,8 @@
         #rx"^(([1-9]+[mps])|([1-7]+z))*( (([1-9]+[mps][1-9]*)|([1-7]+z[1-7]*)))*$"
         (remove-parens s))))
 
+(require/typed racket/contract
+               [#:opaque Contract contract?]
+               [or/c (-> Contract * Contract)])
 (define (maybe/c c)
-  (or/c #f c))
+  (or/c (assert c contract?)))
