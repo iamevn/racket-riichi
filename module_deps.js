430e02066ca7a2176a6ba0fdd3a0af0ac8266322
iamevn <iamevn@users.noreply.github.com>
2021-05-24 20:07:37 -0700

    js goes in svg

--- module_deps.js
+++ module_deps.js
@@ -1,29 +1,32 @@
-let setColor = (color) => 
-  (query) => {
+window.addEventListener('DOMContentLoaded', () => {
+  let setColor = (color) => (query) => {
     for (let n of document.querySelectorAll(query)) {
-      //n.style.stroke = color;
       for (let p of n.querySelectorAll('polygon')) {
         p.style.stroke = color;
+        if (p.parentElement.classList.contains('edge')) {
+          p.style.fill = color;
+        }
       }
       for (let p of n.querySelectorAll('path')) {
         p.style.stroke = color;
       }
     }
   };
-let clear = () => setColor('')('*');
-let highlight = (cn) => setColor('red')('.' + cn);
-
-let _last_selected = '';
-for (const n of document.getElementsByClassName('node')) {
-  const cn = Array.from(n.classList).filter((s) => s != 'node');
-  n.onclick = () => {
-    if (_last_selected == cn) {
-      clear();
-      _last_selected = '';
-    } else {
-      clear();
-      highlight(cn);
-      _last_selected = cn;
-    }
-  };
-}
+  let clear = () => setColor('')('*');
+  let highlight = (cn) => setColor('red')('.' + cn);
+  
+  let _last_selected = '';
+  for (const n of document.getElementsByClassName('node')) {
+    const cn = Array.from(n.classList).filter((s) => s != 'node');
+    n.onclick = () => {
+      if (_last_selected == cn) {
+        clear();
+        _last_selected = '';
+      } else {
+        clear();
+        highlight(cn);
+        _last_selected = cn;
+      }
+    };
+  }
+});
